411366a56a582aa9cdaedc09f3506e15c2b86365
dionysisterziou <dionysisterziou@gmail.com>
2025-06-22 14:11:49 +0300

    Refactor a little bit the spaces

--- dbt/greekmpany/models/employee_count_per_department.sql
+++ dbt/greekmpany/models/employee_count_per_department.sql
@@ -3,8 +3,8 @@
 SELECT
 	d.department_name,
 	COUNT(e.employee_id) AS total_employees
-FROM greekmpany.employee e
-INNER JOIN greekmpany.department d
+FROM greekmpany.employee AS e
+INNER JOIN greekmpany.department AS d
 	ON e.department_id = d.department_id
 GROUP BY d.department_id
 ORDER BY total_employees DESC
