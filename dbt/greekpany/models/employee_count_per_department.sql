1b03fbc0d4d7f211abe74a4b9c9662895a556eb7
dionysisterziou <dionysisterziou@gmail.com>
2025-07-27 17:22:47 +0300

    Change invalid names

--- dbt/greekpany/models/employee_count_per_department.sql
+++ dbt/greekpany/models/employee_count_per_department.sql
@@ -1,10 +1,10 @@
 {{ config(materialized='view') }}
 
 SELECT
- d.department_name,
- COUNT(e.employee_id) AS total_employees
+  d.department_name,
+  COUNT(e.id) AS total_employees
 FROM greekpany.employee AS e
 INNER JOIN greekpany.department AS d
- ON e.department_id = d.department_id
-GROUP BY d.department_id
+  ON e.id = d.id
+GROUP BY d.id
 ORDER BY total_employees DESC
