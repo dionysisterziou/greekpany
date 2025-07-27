1d69890fc7e7141c591fd52086443d415642bd5f
dionysisterziou <dionysisterziou@gmail.com>
2025-07-27 21:53:18 +0300

    Refactor model to use CTE

--- dbt/greekpany/models/employee_count_per_department.sql
+++ dbt/greekpany/models/employee_count_per_department.sql
@@ -1,10 +1,28 @@
 {{ config(materialized='view') }}
 
-SELECT
-  d.department_name,
-  COUNT(e.id) AS total_employees
-FROM greekpany.employee AS e
-INNER JOIN greekpany.department AS d
-  ON e.id = d.id
-GROUP BY d.id
+WITH department AS (
+  SELECT
+    id,
+    department_name
+  FROM greekpany.department
+),
+employee AS (
+  SELECT
+    id,
+    department_id
+  FROM greekpany.employee
+),
+
+final AS (
+  SELECT
+    d.department_name AS department,
+    COUNT(e.id) AS total_employees
+  FROM employee e
+  INNER JOIN department d
+    ON e.department_id = d.id
+  GROUP BY d.department_name
+)
+
+SELECT *
+FROM final
 ORDER BY total_employees DESC
